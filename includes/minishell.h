e528f5adf9ce42d74be2417caaa7a480f21f88b5
Frederico Meira <fmeira@c1r19s3.42lisboa.com>
2021-11-07 00:59:32 +0000

    heder with new structs

--- includes/minishell.h
+++ includes/minishell.h
@@ -5,6 +5,12 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
+typedef enum s_bool
+{
+	false,
+	true
+}			t_bool;
+
 typedef struct s_cmd
 {
 	char	**cmdline;
@@ -12,11 +18,28 @@ typedef struct s_cmd
 	int		i;
 }			t_cmd;
 
+typedef	struct s_elist
+{
+	char			*key;
+	char			*value;
+	struct s_elist	*next;
+}			t_elist;
+
+typedef struct s_data
+{
+	t_bool	has_pipe;
+	t_cmd	*cmd;
+	t_elist *env_list;
+}			t_data;
+
 t_cmd *go;
 
 char	**split(char const *s, char c, t_cmd *d);
 int		check_cmd(char *str, t_cmd *d, char **envp);
 void	exit_func(t_cmd *d);
 void	do_env(char **envp);
+//void	check_quotes(char **str, t_env *env);
+t_elist	*set_env_list(char **envp);
+
 
 #endif
