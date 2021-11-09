58fc7d9016b8b8cee7beb9cc5e65a8c185108026
Frederico Meira <fmeira@c1r4s5.42lisboa.com>
2021-11-09 19:16:27 +0000

    pseudo code for token validation

--- srcs/validate_tokens.c
+++ srcs/validate_tokens.c
@@ -2,6 +2,11 @@
 
 int	validate_export(t_tokens *com)
 {
+	if (next = attribution) //make token for attribution
+	{
+
+	}
+
 
 }
 
@@ -36,28 +41,3 @@ t_bool	validate_tokens(t_tokens *tkn)
 	}
 	return (0);
 }
-
-// static void	parse_quotes(char **str, char c, t_data *data, int pos)
-// {
-// 	if (c = "'")
-// 		str[i] = ft_str
-// }
-
-// void	check_quotes(char **str, t_data *data)
-// {
-// 	int	i;
-// 	int	j;
-
-// 	i = 0;
-// 	while (str[i])
-// 	{
-// 		j = 0;
-// 		while(str[i][j])
-// 		{
-// 			if (str[i] == "'" || str[i] == "\"")
-// 				parse_quotes(str, str[i], data, i);
-// 			j++;
-// 		}
-// 		i++;
-// 	}
-// }
