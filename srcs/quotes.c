ccf11657b709253c5aee3165a6e1e34dc93f4c3f
Frederico Meira <fmeira@c1r19s4.42lisboa.com>
2021-11-10 21:15:09 +0000

    quotes ending parsing

--- srcs/quotes.c
+++ srcs/quotes.c
@@ -1,95 +1,137 @@
 #include "../includes/minishell.h"
 
-static int	check_q_count(t_tokens *tkn, char aspas)
+static int check_end(char *str, char aspas, int *quotes_exist, int *j)
 {
-	int			q_counter;
-	t_tokens	*curr;
-	int			i;
-	int			to_malloc;
-
-	curr = tkn;
-	q_counter = 0;
-	i = 0;
-	to_malloc = 0;
-	while (curr)
+	*quotes_exist = *quotes_exist++;
+	while (str[*j] != aspas)
 	{
-		while(curr->str[i])
+		if (str[*j] == '\0')
+			return (1);
+		*j = *j++;
+	}
+	return (0);
+}
+
+static int	open_qs(char **str)
+{
+	int	quotes_exist;
+	int	i;
+	int	j;
+
+	quotes_exist = 0;
+	i = -1;
+	while (str[++i])
+	{
+		j = 0;
+		while (str[i][j])
 		{
-			if (curr->str[i] == aspas)
-				q_counter++;
-			else if (curr->str[i] != aspas || (curr->str[i] == aspas
-					&& (q_counter % 2)))
-				to_malloc++;
+			if (str[i][j] == '\'')
+			{
+				if (check_end((str[i][j]), '\'', &quotes_exist, &j))
+					return (1);
+			}
+			else if (str[i][j] == '"')
+			{
+				if (check_end((str[i][j]), '"', &quotes_exist, &j))
+					return (1);
+			}
+			j++;
+		}
+	}
+	return (quotes_exist);
+}
+
+int	q_find(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != ' ')
+	{
+		if (str[i] == "'" || str[i] == "'")
+	}
+}
+
+int	trim_quotes(char **str)
+{
+	int	i;
+	int	j;
+	int	check_quotes;
+
+	i = 0;
+	j = 0;
+	check_quotes = open_qs(str);
+	if (check_quotes == 1)
+		return ((printf("error: open quotes\n")));
+	else if (!check_quotes)
+		return (0);
+	if (q_find(str[i]))
+	{
+		free(str[i]);
+
+	while (str[i])
+	{
+		if (str[i] == echo)
+		{
+			j = 0;
+			while (str[i][j])
+			{
+				if (str[i][j] == '"')
+				{
+					if (parse_dbquotes(str, &i))
+						return (1);
+				}
+				else if (str[i][j] == "'")
+				{
+					if (parse_sgquotes(str, &i))
+						return (1);
+				}
+				j++;
+			}
 			i++;
 		}
-		curr = curr->next;
 	}
-	if (q_counter % 2)
-	{
-		printf("error: open quotes\n");
-		return (1);
-	}
-	return (to_malloc);
+	return (0);
 }
+
+
 
 static int	parse_dbquotes(t_tokens	**tkn, int i)
-{
-	// t_tokens	*curr;
-	// char		*newstr;
-	// t_bool		iter;
-	// int			pos;
+// {
+// 	// t_tokens	*curr;
+// 	// char		*newstr;
+// 	// t_bool		iter;
+// 	// int			pos;
 
-	// curr = *tkn;
-	// iter = true;
-	// pos = i + 1;
-	// newstr = NULL;
-	// while (iter && curr)
-	// {
-	// 	if (curr->str[pos] != '"')
-	// 	{
-	// 		newstr = append_char(newstr, check_q_count(curr));
-	// 	}
-	// }
-	int i = 0;
-	int x = 0;
-	int j = 0;
-	char *str;
-	char *also_str;
+// 	// curr = *tkn;
+// 	// iter = true;
+// 	// pos = i + 1;
+// 	// newstr = NULL;
+// 	// while (iter && curr)
+// 	// {
+// 	// 	if (curr->str[pos] != '"')
+// 	// 	{
+// 	// 		newstr = append_char(newstr, check_q_count(curr));
+// 	// 	}
+// 	// }
+// 	int i = 0;
+// 	int x = 0;
+// 	int j = 0;
+// 	char *str;
+// 	char *also_str;
 
-	str = (*tkn)->str;
-	also_str = (*tkn)->str;
-	while(also_str[j] != '\0')
-	{
-		while(also_str[j] == '"')
-		{
-			j++;
-			x++;
-		}
-		(*tkn)->str[i] = str[i + x];
-		i++;
-		j++;
-	}
-	(*tkn)->str[i] = '\0';
-}
-
-void	check_quotes(t_data *data)
-{
-	t_tokens	*curr;
-	int			i;
-
-	i = 0;
-	curr = data->token_list;
-	while (curr)
-	{
-		while(curr->str[i])
-		{
-			if (curr->str[i] == "'")
-			{
-				if (parse_dbquotes(&curr, i)
-
-			else if (curr->str[i] == '"')
-				i = parse_sgquotes(&curr, i);
-		}
-		i++;
-	}
-}
+// 	str = (*tkn)->str;
+// 	also_str = (*tkn)->str;
+// 	while(also_str[j] != '\0')
+// 	{
+// 		while(also_str[j] == '"')
+// 		{
+// 			j++;
+// 			x++;
+// 		}
+// 		(*tkn)->str[i] = str[i + x];
+// 		i++;
+// 		j++;
+// 	}
+// 	(*tkn)->str[i] = '\0';
+// }
