d0c31bd343872eddbc5c1ad7c3cd4f82263a91ed
Joao Arsenio <jarsenio@c1r1s6.42lisboa.com>
2021-12-04 19:54:07 +0000

    makefile working with readline

--- srcs/parsing/check_cmd.c
+++ srcs/parsing/check_cmd.c
@@ -9,9 +9,9 @@ void	check_cmd(t_data *d, t_tokens *t)
 	if (ft_strcmp(t->str, "pwd") == 0)
 		do_pwd();
 	// if (ft_strcmp(t->str, "export") == 0)
-	// 	do_export(d->envars_list, )
+	// 	do_export(d->envars_list, t->next->str);
 	// if (ft_strcmp(t->str, "unset") == 0)
-	// 	do_unset(d->envars_list, )
+	// 	do_unset(d->envars_list, t->next->str);
 	if (ft_strcmp(t->str, "env") == 0)
 		do_env(d->envars_list);
 	if (ft_strcmp(t->str, "exit") == 0)
--- srcs/utils/signals.c
+++ srcs/utils/signals.c
@@ -3,6 +3,7 @@
 static void	ctrl_c_handler()
 {
 	write(1, "\n", 1);
+	rl_replace_line("", 0);
 	rl_on_new_line();
 	rl_redisplay();
 }
@@ -11,10 +12,12 @@ void	do_i_have_signal()
 {
 	struct sigaction ctrl_c;
 	struct sigaction ctrl_barra;
+	// struct sigaction ctrl_d;
 
-	ctrl_c.sa_flags = SA_RESTART;
+	// ctrl_d.sa_handler = &exit_func;
 	ctrl_c.sa_handler = &ctrl_c_handler;
-	sigaction(SIGINT, &ctrl_c, NULL);
+	ctrl_c.sa_flags = SA_RESTART;
 	ctrl_barra.sa_handler = SIG_IGN;
+	sigaction(SIGINT, &ctrl_c, NULL);
 	sigaction(SIGQUIT, &ctrl_barra, NULL);
 }
