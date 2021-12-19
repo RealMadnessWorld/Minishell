ed477750e0ce96e0de1cd5bb148fc371e645b9b0
Frederico Meira <fmeira@c1r12s4.42lisboa.com>
2021-12-19 18:14:02 +0000

    exit func with proper exit codes

--- srcs/exec/builtin_exec.c
+++ srcs/exec/builtin_exec.c
@@ -21,8 +21,8 @@ int	do_builtin(t_data *d, t_tokens *t)
 		return (do_env(d->envars_list));
 	if (ft_strcmp(t->str, "exit") == 0)
 	{
-		exit_func(d);
-		return (0);
+		exit_func(d, t);
+		return (1);
 	}
 	return (2);
 }
